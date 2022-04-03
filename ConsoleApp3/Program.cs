9e20dce1b3a7310e886322b6d19952d1f670fe23
vlasvv <rozhdestvenskiy87@gmail.com>
2022-04-03 18:31:31 +0400

    Первые успехи

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,8 +2,18 @@
 {
     static void Main(string[] args)
     {
-        string MyName;
-        MyName = "Jane";
-        Console.WriteLine(MyNames);
+        const string MyName = "Jane";
+        Console.WriteLine(MyName);
+        Console.WriteLine("\t Привет, мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is Jane");
+        Console.WriteLine("\t My name is \n Jane");
+        Console.WriteLine('\x23');
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+
+
+
+        Console.ReadKey();
     }
 }
