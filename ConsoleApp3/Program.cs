0eb48ad14b87b374aabc8dc3ab146ad58d1bde7f
vlasvv <rozhdestvenskiy87@gmail.com>
2022-04-12 19:12:44 +0400

    Сбор Биографии

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,21 +1,9 @@
-﻿class Programm
-{
-    
-    static void Main(string[] args) {
-        if (Console.BackgroundColor == ConsoleColor.Black)
-        {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.Clear();
-        }
-
-
-
-        }
-    enum Semaphore
-    {
-        Green = 100,
-        Yellow = 200,
-        Red = 300,
-    }
-}
+﻿Console.WriteLine("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.WriteLine("Введите ваш возраст: ");
+byte age = (byte)int.Parse(Console.ReadLine());
+Console.WriteLine("Вас зовут {0} и вам {1} года(лет)", name, age);
+Console.WriteLine("Введите дату вашего Дня Рождения: ");
+string birthday = Console.ReadLine();
+Console.WriteLine("Дата вашего дня рождения: {0}", birthday);
+Console.ReadKey();
