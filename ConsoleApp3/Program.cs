867b19cd2ca3bdecdffde4a4ce3bf383f2633614
vlasvv <rozhdestvenskiy87@gmail.com>
2022-04-13 14:21:14 +0400

    Задание 4.1.17

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,9 +1,32 @@
-﻿Console.WriteLine("Введите ваше имя: ");
-string name = Console.ReadLine();
-Console.WriteLine("Введите ваш возраст: ");
-byte age = (byte)int.Parse(Console.ReadLine());
-Console.WriteLine("Вас зовут {0} и вам {1} года(лет)", name, age);
-Console.WriteLine("Введите дату вашего Дня Рождения: ");
-string birthday = Console.ReadLine();
-Console.WriteLine("Дата вашего дня рождения: {0}", birthday);
-Console.ReadKey();
+﻿Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+var color = Console.ReadLine();
+
+switch (color)
+{
+	case "red":
+		Console.BackgroundColor = ConsoleColor.Red;
+		Console.ForegroundColor = ConsoleColor.Black;
+
+		Console.WriteLine("Your color is red!");
+		break;
+
+	case "green":
+		Console.BackgroundColor = ConsoleColor.Green;
+		Console.ForegroundColor = ConsoleColor.Black;
+
+		Console.WriteLine("Your color is green!");
+		break;
+
+	case "cyan":
+		Console.BackgroundColor = ConsoleColor.Cyan;
+		Console.ForegroundColor = ConsoleColor.Black;
+		Console.WriteLine("Your color is cyan!");
+		break;
+	default:
+		Console.BackgroundColor = ConsoleColor.Yellow;
+		Console.ForegroundColor = ConsoleColor.Red;
+
+		Console.WriteLine("Your color is yellow!");
+		break;
+}
