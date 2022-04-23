54128bf8ca7c48128118f2cc0f9910d0d5786fd6
vlasvv <rozhdestvenskiy87@gmail.com>
2022-04-23 11:58:05 +0400

    Анкетирование

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,32 +1,35 @@
-﻿Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-var color = Console.ReadLine();
-
-switch (color)
+﻿class Programm
 {
-	case "red":
-		Console.BackgroundColor = ConsoleColor.Red;
-		Console.ForegroundColor = ConsoleColor.Black;
 
-		Console.WriteLine("Your color is red!");
-		break;
+    static void Main(string[] args)
+    {
+        (string Name, string LastName, string Login, int LoginLength, bool Pet, string[] colors, int Age) User;
+        Console.WriteLine("Введите своё имя: ");
+        User.Name = Console.ReadLine();
+        Console.WriteLine("Введите фамилию: ");
+        User.LastName = Console.ReadLine();
+        Console.WriteLine("Введите логин: ");
+        User.Login = (Console.ReadLine());
+        Console.WriteLine("Длина Логина: {0}", User.Login.Length);
+        Console.WriteLine("Есть ли у вас животные? Да или Нет");
+        var result = Console.ReadLine();
+        if (result == "Да")
+        {
+            User.Pet = true;
+        }
+        else
+        {
+            User.Pet = false;
+        }
+        Console.WriteLine("Введите возраст пользователя: ");
+        User.Age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите ваши 3 любимых цвета ");
+        User.colors = new string[3];
+        for (int i = 0; i < User.colors.Length; i++)
+        {
+            User.colors[i] = Console.ReadLine();
+        }
+    }
 
-	case "green":
-		Console.BackgroundColor = ConsoleColor.Green;
-		Console.ForegroundColor = ConsoleColor.Black;
-
-		Console.WriteLine("Your color is green!");
-		break;
-
-	case "cyan":
-		Console.BackgroundColor = ConsoleColor.Cyan;
-		Console.ForegroundColor = ConsoleColor.Black;
-		Console.WriteLine("Your color is cyan!");
-		break;
-	default:
-		Console.BackgroundColor = ConsoleColor.Yellow;
-		Console.ForegroundColor = ConsoleColor.Red;
-
-		Console.WriteLine("Your color is yellow!");
-		break;
 }
+
